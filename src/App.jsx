bec201be9b1773d78f26174845e6e05ffd945abd
seeratkawr <sman833@aucklanduni.ac.nz>
2025-07-19 15:47:16 +1200

    Fixed the screen size to phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
-import FeedPage from "./pages/FeedPage"
-import ProfilePage from "./pages/ProfilePage"
-import BottomNav from "./components/BottomNav"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import FeedPage from "./pages/FeedPage";
+import ProfilePage from "./pages/ProfilePage";
+import BottomNav from "./components/BottomNav";
 import "./styles/App.css";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<FeedPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-      <BottomNav />s
-    </>
-  )
+    <div className="flex items-center justify-center w-screen h-screen bg-gray-100 overflow-hidden">
+      <div className="relative w-[375px] h-[667px] bg-blue-100 rounded-xl shadow-2xl overflow-y-auto border-2 border-blue-400">
+        {/* Routes */}
+        <div className="pb-16">
+          <Routes>
+            <Route path="/" element={<FeedPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </div>
+
+        {/* Bottom Navigation */}
+        <div className="absolute bottom-0 w-full">
+          <BottomNav />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
