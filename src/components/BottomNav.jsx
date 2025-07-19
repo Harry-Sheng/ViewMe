be5144a1e04df61c5e691b35e3c0f656d4d41092
seeratkawr <sman833@aucklanduni.ac.nz>
2025-07-20 10:29:35 +1200

    changed settings icon to user icon in nav bar

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,14 +1,14 @@
-import React from "react"
-import { Link, useLocation } from "react-router-dom"
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function BottomNav() {
-  const location = useLocation()
+  const location = useLocation();
 
   const navItems = [
     { to: "/", icon: "/home-but-white.png", label: "Home" },
     { to: "/search", icon: "/search-white.png", label: "Search" },
-    { to: "/profile", icon: "/setting-icon-white.png", label: "Profile" },
-  ]
+    { to: "/profile", icon: "/user-white.png", label: "Profile" },
+  ];
 
   return (
     <nav className="bg-[#2A324B] text-white flex justify-around items-center h-12 border-t border-neutral-800 rounded-b-xl">
@@ -24,7 +24,7 @@ function BottomNav() {
         </Link>
       ))}
     </nav>
-  )
+  );
 }
 
-export default BottomNav
+export default BottomNav;
